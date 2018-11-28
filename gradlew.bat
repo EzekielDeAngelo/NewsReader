c48d584e5dc8c21fde83d5447a08f413a74e9a6c
EzekielDeAngelo <anthony.p777@gmail.com>
2018-11-28 17:10:32 +0100

    Tests

--- gradlew.bat
+++ gradlew.bat
@@ -17,7 +17,7 @@ set APP_HOME=%DIRNAME%
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+if defined JAVA_HOME goto findJavaFromJavaHom
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
