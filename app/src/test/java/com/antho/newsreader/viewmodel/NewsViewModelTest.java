233463dc455181258ccb121c3d2315b1e0cc6d21
EzekielDeAngelo <anthony.p777@gmail.com>
2019-03-26 21:46:06 +0100

    Unit tests

--- app/src/test/java/com/antho/newsreader/viewmodel/NewsViewModelTest.java
+++ app/src/test/java/com/antho/newsreader/viewmodel/NewsViewModelTest.java
@@ -1,14 +1,35 @@
 package com.antho.newsreader.viewmodel;
 
+import com.antho.newsreader.db.NewsService;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.mockito.Mockito.spy;
 
 public class NewsViewModelTest
 {
     private NewsViewModel newsViewModel;
+    @Mock
+    private NewsService service;
 
     @Before
     public void setUp() throws Exception
     {
-        newsViewModel = new NewsViewModel("");
+        MockitoAnnotations.initMocks(this);
+
+        newsViewModel = spy(new NewsViewModel(""));
+
     }
+    @Test
+    public void test()
+    {
+
+    }
+    @Spy
+    NewsViewModel viewModel = new NewsViewModel("");
+
 }
